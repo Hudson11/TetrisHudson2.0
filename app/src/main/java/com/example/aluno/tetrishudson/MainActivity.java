f34624226fb9d88e8e4a832b472284d266b0f178
fbio1 <fbiohenrique1@gmail.com>
2018-08-22 15:07:05 -0300

    Initial commit

---
+++ app/src/main/java/com/example/aluno/tetrishudson/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.aluno.tetrishudson;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void onClick(View v){
+        Intent intent = new Intent(MainActivity.this, ResultActivity.class);
+        startActivity(intent);
+    }
+
+    public void settings(View v){
+        Intent intent = new Intent(MainActivity.this, ConfigActivity.class);
+        startActivity(intent);
+    }
+}
