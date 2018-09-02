c48d780ca01ce83c78d08581c65da3b40f56751d
hudson11 <hudsonhdj36@gmail.com>
2018-09-02 16:30:15 -0300

    Versão otimizada, Usa de ViewModel ainda não está completo.

--- app/src/main/java/com/example/aluno/tetrishudson/Activity_class/GameActivity.java
+++ app/src/main/java/com/example/aluno/tetrishudson/Activity_class/GameActivity.java
@@ -1,6 +1,7 @@
 package com.example.aluno.tetrishudson.Activity_class;
 
 import android.arch.lifecycle.ViewModel;
+import android.arch.lifecycle.ViewModelProviders;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
@@ -33,60 +34,59 @@ public class GameActivity extends AppCompatActivity {
     boolean nova_peça = true;
 
     ImageView[][] borda;
-    ImageView imagem;
+    ImageView[][] proxima_peça;
+
     int[][] referencia;
-    int[][] fundo;
 
     GridLayout layout;
-    TextView pontos;
+    GridLayout layout_prox;
 
     //Configuração Padrão, vale caso seja instalado do 0 e o usuário ainda não tem configurado as dificuldades do jogo.
     int velocidade = 400, nivel = 4;
 
-    Pecas peça_atual;
-
-    Handler handle = new Handler();
-    boolean repete = true;
-
     //CRIANDO A CLASSE PARA SALVAR O ESTADO DA APLICAÇÂO
     public static class viewModel extends ViewModel{
-        int[][] estado = new int[EIXO_X][EIXO_Y];
+        int[][] fundo = new int[EIXO_X][EIXO_Y];
+        Pecas peça_atual;
+        Handler handle = new Handler();
+        boolean repete = true;
+        TextView pontos;
+        ImageView imagem;
     }
 
-    viewModel viewModel = new viewModel();
+    viewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
+        viewModel = ViewModelProviders.of(this).get(viewModel.class);
 
         //Recebe informações de Configurações selecionadas em ConfigActivity
         SharedPreferences prefs = getSharedPreferences("PREFS_NAME", MODE_PRIVATE);
         velocidade = prefs.getInt("velocidade", 1);
         nivel = prefs.getInt("nivel", 2);
 
-        //checar se as informações foram corretamente passadas
-        Log.i("JOGO", "velocidade: " + velocidade + " nivel: " + nivel);
-
-
-        pontos = findViewById(R.id.pontos);
-        imagem = findViewById(R.id.proxima);
-
         //matriz de referências
         referencia = new int[EIXO_X][EIXO_Y];
 
         //matriz para referencia das peças do fundo
-        fundo = new int[EIXO_X][EIXO_Y];
+        //fundo = new int[EIXO_X][EIXO_Y];
 
         //Matriz de Imagem
         //Tabuleiro
         borda = new ImageView[EIXO_X][EIXO_Y];
+        proxima_peça = new ImageView[4][4];
+
         layout = findViewById(R.id.gridBoard);
+        layout_prox = findViewById(R.id.grid);
 
         //Set linhas e colunas
         layout.setRowCount(EIXO_X);
         layout.setColumnCount(EIXO_Y);
+        layout_prox.setRowCount(4);
+        layout_prox.setColumnCount(4);
 
         LayoutInflater inflater = LayoutInflater.from(this);
 
@@ -109,12 +109,19 @@ public class GameActivity extends AppCompatActivity {
                     referencia[x][y] = 1;
                 }else{
                     referencia[x][y] = -1;
-                    fundo[x][y] = -1;
+                    viewModel.fundo[x][y] = -1;
                 }
                 //Adicionando ao Grid Layout o conteúdo no tabuleiro de imagens na posição X,Y.
                 layout.addView(borda[x][y]);
             }
         }
+
+       for(int i = 0; i < 4; i++){
+            for(int j = 0; j < 4; j++){
+                proxima_peça[i][j] = (ImageView) inflater.inflate(R.layout.inflate_image_black, layout_prox, false);
+                layout_prox.addView(proxima_peça[i][j]);
+            }
+       }
 
         Movimenta();
     }
@@ -131,58 +138,57 @@ public class GameActivity extends AppCompatActivity {
             @Override
             public void run() {
 
-                repete = true;
+                viewModel.repete = true;
 
-                while(repete){
+                while(viewModel.repete){
                     try{
                         Thread.sleep(velocidade);
                     }catch(InterruptedException e){
                         e.printStackTrace();
                     }
 
-                    handle.post(new Runnable() {
+                    viewModel.handle.post(new Runnable() {
                         @Override
                         public void run() {
+                            if(nova_peça == true) {
 
-                          if(nova_peça == true) {
+                                atual_number = createRadom_Peça();
+                                radom = createRadom_Peça();
 
-                          atual_number = createRadom_Peça();
-                          radom = createRadom_Peça();
+                                if (atual_number == 0) {
+                                    viewModel.peça_atual = new Peca_1();
+                                } else if (atual_number == 1) {
+                                    viewModel.peça_atual = new Peca_2();
+                                } else if (atual_number == 2) {
+                                    viewModel.peça_atual = new Peca_3();
+                                } else if (atual_number == 3) {
+                                    viewModel.peça_atual = new Peca_4();
+                                } else if (atual_number == 4) {
+                                    viewModel.peça_atual = new Peca_5();
+                                } else if (atual_number == 5) {
+                                    viewModel.peça_atual = new Peca_6();
+                                } else {
+                                    viewModel.peça_atual = new Peca_7();
+                                }
 
-                          if (atual_number == 0) {
-                              peça_atual = new Peca_1();
-                          } else if (atual_number == 1) {
-                              peça_atual = new Peca_2();
-                          } else if (atual_number == 2) {
-                              peça_atual = new Peca_3();
-                          } else if (atual_number == 3) {
-                              peça_atual = new Peca_4();
-                          } else if (atual_number == 4) {
-                              peça_atual = new Peca_5();
-                          } else if (atual_number == 5) {
-                               peça_atual = new Peca_6();
-                          } else {
-                               peça_atual = new Peca_7();
-                          }
-                            nova_peça = false;
-                              //peça_atual =  new Peca_5();
-                          }
+                                nova_peça = false;
+                            }
 
-                            peça_atual.getPontos().get(0)[0] += 1;
-                            peça_atual.getPontos().get(1)[0] += 1;
-                            peça_atual.getPontos().get(2)[0] += 1;
-                            peça_atual.getPontos().get(3)[0] += 1;
+                            viewModel.peça_atual.getPontos().get(0)[0] += 1;
+                            viewModel.peça_atual.getPontos().get(1)[0] += 1;
+                            viewModel.peça_atual.getPontos().get(2)[0] += 1;
+                            viewModel.peça_atual.getPontos().get(3)[0] += 1;
 
-                            referencia[peça_atual.getPontos().get(0)[0]][peça_atual.getPontos().get(0)[1]] = 9;
-                            referencia[peça_atual.getPontos().get(1)[0]][peça_atual.getPontos().get(1)[1]] = 9;
-                            referencia[peça_atual.getPontos().get(2)[0]][peça_atual.getPontos().get(2)[1]] = 9;
-                            referencia[peça_atual.getPontos().get(3)[0]][peça_atual.getPontos().get(3)[1]] = 9;
+                            referencia[viewModel.peça_atual.getPontos().get(0)[0]][viewModel.peça_atual.getPontos().get(0)[1]] = 9;
+                            referencia[viewModel.peça_atual.getPontos().get(1)[0]][viewModel.peça_atual.getPontos().get(1)[1]] = 9;
+                            referencia[viewModel.peça_atual.getPontos().get(2)[0]][viewModel.peça_atual.getPontos().get(2)[1]] = 9;
+                            referencia[viewModel.peça_atual.getPontos().get(3)[0]][viewModel.peça_atual.getPontos().get(3)[1]] = 9;
 
 
                             for(int i = 1; i < EIXO_X - 1; i++){
-                                for(int j = 1; j < EIXO_Y -1; j++){
+                                for(int j = 1; j < EIXO_Y - 1; j++){
 
-                                    if(referencia[i][j] != 9 && fundo[i][j] == -1){
+                                    if(referencia[i][j] != 9 && viewModel.fundo[i][j] == -1){
                                         borda[i][j].setImageResource(R.drawable.blocoblackp);
                                     }else{
                                         if(radom == 0)
@@ -203,23 +209,23 @@ public class GameActivity extends AppCompatActivity {
                                 }
                             }
 
-                            referencia[peça_atual.getPontos().get(0)[0]][peça_atual.getPontos().get(0)[1]] = 0;
-                            referencia[peça_atual.getPontos().get(1)[0]][peça_atual.getPontos().get(1)[1]] = 0;
-                            referencia[peça_atual.getPontos().get(2)[0]][peça_atual.getPontos().get(2)[1]] = 0;
-                            referencia[peça_atual.getPontos().get(3)[0]][peça_atual.getPontos().get(3)[1]] = 0;
+                            referencia[viewModel.peça_atual.getPontos().get(0)[0]][viewModel.peça_atual.getPontos().get(0)[1]] = 0;
+                            referencia[viewModel.peça_atual.getPontos().get(1)[0]][viewModel.peça_atual.getPontos().get(1)[1]] = 0;
+                            referencia[viewModel.peça_atual.getPontos().get(2)[0]][viewModel.peça_atual.getPontos().get(2)[1]] = 0;
+                            referencia[viewModel.peça_atual.getPontos().get(3)[0]][viewModel.peça_atual.getPontos().get(3)[1]] = 0;
 
-                            if (referencia[peça_atual.getPontos().get(3)[0] + 1][peça_atual.getPontos().get(3)[1]] == 1
-                                    || fundo[peça_atual.getPontos().get(3)[0]+ 1][peça_atual.getPontos().get(3)[1]] != -1
-                                    || fundo[peça_atual.getPontos().get(2)[0]+ 1][peça_atual.getPontos().get(2)[1]] != -1
-                                    || fundo[peça_atual.getPontos().get(1)[0]+ 1][peça_atual.getPontos().get(1)[1]] != -1
-                                    || fundo[peça_atual.getPontos().get(0)[0]+ 1][peça_atual.getPontos().get(0)[1]] != -1){
+                            if (referencia[viewModel.peça_atual.getPontos().get(3)[0] + 1][viewModel.peça_atual.getPontos().get(3)[1]] == 1
+                                    || viewModel.fundo[viewModel.peça_atual.getPontos().get(3)[0]+ 1][viewModel.peça_atual.getPontos().get(3)[1]] != -1
+                                    || viewModel.fundo[viewModel.peça_atual.getPontos().get(2)[0]+ 1][viewModel.peça_atual.getPontos().get(2)[1]] != -1
+                                    || viewModel.fundo[viewModel.peça_atual.getPontos().get(1)[0]+ 1][viewModel.peça_atual.getPontos().get(1)[1]] != -1
+                                    || viewModel.fundo[viewModel.peça_atual.getPontos().get(0)[0]+ 1][viewModel.peça_atual.getPontos().get(0)[1]] != -1){
 
                                 nova_peça = true;
 
-                                fundo[peça_atual.getPontos().get(0)[0]][peça_atual.getPontos().get(0)[1]] = radom;
-                                fundo[peça_atual.getPontos().get(1)[0]][peça_atual.getPontos().get(1)[1]] = radom;
-                                fundo[peça_atual.getPontos().get(2)[0]][peça_atual.getPontos().get(2)[1]] = radom;
-                                fundo[peça_atual.getPontos().get(3)[0]][peça_atual.getPontos().get(3)[1]] = radom;
+                                viewModel.fundo[viewModel.peça_atual.getPontos().get(0)[0]][viewModel.peça_atual.getPontos().get(0)[1]] = radom;
+                                viewModel.fundo[viewModel.peça_atual.getPontos().get(1)[0]][viewModel.peça_atual.getPontos().get(1)[1]] = radom;
+                                viewModel.fundo[viewModel.peça_atual.getPontos().get(2)[0]][viewModel.peça_atual.getPontos().get(2)[1]] = radom;
+                                viewModel.fundo[viewModel.peça_atual.getPontos().get(3)[0]][viewModel.peça_atual.getPontos().get(3)[1]] = radom;
                             }
                         }
                     });
@@ -238,26 +244,48 @@ public class GameActivity extends AppCompatActivity {
 
     public void peça_left(View v){
         ImageButton button = findViewById(R.id.imageButton);
-        peça_atual.left();
+
+        if(referencia[viewModel.peça_atual.getPontos().get(0)[0]][viewModel.peça_atual.getPontos().get(0)[1] - 1] == 1
+        || referencia[viewModel.peça_atual.getPontos().get(1)[0]][viewModel.peça_atual.getPontos().get(1)[1] - 1] == 1
+        || referencia[viewModel.peça_atual.getPontos().get(2)[0]][viewModel.peça_atual.getPontos().get(2)[1] - 1] == 1
+        || referencia[viewModel.peça_atual.getPontos().get(3)[0]][viewModel.peça_atual.getPontos().get(3)[1] - 1] == 1){
+            return;
+        }
+
+        viewModel.peça_atual.left();
         return;
     }
 
     public void peça_right(View v){
         ImageButton button = findViewById(R.id.imageButton2);
-        peça_atual.right();
+
+        if(referencia[viewModel.peça_atual.getPontos().get(0)[0]][viewModel.peça_atual.getPontos().get(0)[1] + 1] == 1
+        || referencia[viewModel.peça_atual.getPontos().get(1)[0]][viewModel.peça_atual.getPontos().get(1)[1] + 1] == 1
+        || referencia[viewModel.peça_atual.getPontos().get(2)[0]][viewModel.peça_atual.getPontos().get(2)[1] + 1] == 1
+        || referencia[viewModel.peça_atual.getPontos().get(3)[0]][viewModel.peça_atual.getPontos().get(3)[1] + 1] == 1){
+            return;
+        }
+        viewModel.peça_atual.right();
         return;
     }
 
     public void peça_rotate(View v){
         ImageButton button = findViewById(R.id.imageButton3);
-        peça_atual.rotate();
+        viewModel.peça_atual.rotate();
         return;
     }
 
     public void movimenta_baixo(View v){
         ImageButton button = findViewById(R.id.imageButton4);
-        peça_atual.Movimenta_baixo();
+
+        if(referencia[viewModel.peça_atual.getPontos().get(0)[0] + 1][viewModel.peça_atual.getPontos().get(0)[1]] == 1
+        || referencia[viewModel.peça_atual.getPontos().get(1)[0] + 1][viewModel.peça_atual.getPontos().get(1)[1]] == 1
+        || referencia[viewModel.peça_atual.getPontos().get(2)[0] + 1][viewModel.peça_atual.getPontos().get(2)[1]] == 1
+        || referencia[viewModel.peça_atual.getPontos().get(3)[0] + 1][viewModel.peça_atual.getPontos().get(3)[1]] == 1){
+            return;
+        }
+
+        viewModel.peça_atual.Movimenta_baixo();
         return;
     }
-
 }
